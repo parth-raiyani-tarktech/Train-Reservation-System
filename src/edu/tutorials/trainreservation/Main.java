3121a92c8445d5a7dd7cfdb4f1fce43cc5144227
DilipKhunti <khunti2005@gmail.com>
2024-09-04 17:33:53 +0530

    Update Main.java

--- src/edu/tutorials/trainreservation/Main.java
+++ src/edu/tutorials/trainreservation/Main.java
@@ -10,6 +10,8 @@ import edu.tutorials.trainreservation.reader.TrainSearchRequestReader;
 import edu.tutorials.trainreservation.service.TicketBookingService;
 import edu.tutorials.trainreservation.service.TrainService;
 
+import static edu.tutorials.trainreservation.utils.Constants.SPACE;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,14 +24,22 @@ public class Main {
         TicketBookingService ticketBookingService = new TicketBookingService(trainService);
 
         Scanner sc = new Scanner(System.in);
-        while(true) {
-            System.out.println("\nEnter train search request (type 'exit' to quit)");
+        while (true) {
+            System.out.println(
+                    "\nEnter train search request \nOR \nEnter PNR to see ticket detail by PNR \nOR \nEnter \"EXIT\" to exit \nOR \nEnter \"REPORT\" to generate booking report");
 
             String input = sc.nextLine();
-            if(input.equals("exit")) {
-                break;
-            }
 
+            if (isSingleArg(input)) {
+                if (input.equals("EXIT")) {
+                    break;
+                } else if (input.equals("REPORT")) {
+                    displayTickets(ticketBookingService.getBookedTickets());
+                } else {
+                    System.out.println(ticketBookingService.getTicketByPnr(input));
+                }
+                continue;
+            }
             // Search Trains
             TrainSearchRequest trainSearchRequest = TrainSearchRequestReader.read(input);
             System.out.println(trainSearchRequest + "\n");
@@ -37,10 +47,10 @@ public class Main {
             List<Train> trainsForRoute = trainService.findTrains(trainSearchRequest);
             displayTrainNo(trainsForRoute);
 
-            if(!trainsForRoute.isEmpty()) {
+            if (!trainsForRoute.isEmpty()) {
                 // Select Train Number
                 String trainNo = DataReader.readTrainNoToBookTicket();
-                Ticket bookedTicket = ticketBookingService.bookTicket(trainNo, trainSearchRequest.getCoachType(), trainSearchRequest.getTravelDate(), trainSearchRequest.getPassengerCount());
+                Ticket bookedTicket = ticketBookingService.bookTicket(trainNo, trainSearchRequest);
                 System.out.println("Ticket booked successfully: " + bookedTicket);
             }
         }
@@ -50,7 +60,8 @@ public class Main {
 
     private static void displayTickets(List<Ticket> bookedTickets) {
         System.out.println("Booked Tickets: ");
-        for(Ticket ticket : bookedTickets) {
+        System.out.println("PNR, TRAIN, DATE, FROM, TO, FARE, SEATS");
+        for (Ticket ticket : bookedTickets) {
             System.out.println(ticket);
         }
     }
@@ -58,16 +69,16 @@ public class Main {
     private static void displayTrainNo(List<Train> trains) {
         System.out.println("Found " + trains.size() + " trains");
 
-        for(Train train : trains) {
+        for (Train train : trains) {
             System.out.println("Train: " + train.getTrainNumber());
         }
     }
 
     private static void displayTrainDetails(List<Train> trains) {
-        for(Train train : trains) {
+        for (Train train : trains) {
             System.out.println("Train: " + train.getTrainNumber());
 
-            for(Coach coach : train.getCoaches()) {
+            for (Coach coach : train.getCoaches()) {
                 System.out.println("Coach: " + coach.getCoachName() + " Type: " + coach.getCoachType());
 
                 for (Seat seat : coach.getSeats()) {
@@ -77,5 +88,12 @@ public class Main {
             }
         }
         System.out.println();
+    }
+
+    private static boolean isSingleArg(String input) {
+        if (input.split(SPACE).length == 1) {
+            return true;
+        }
+        return false;
     }
 }
